7525e1299ffc03bcca82ed24d52cc0a045054194
Prakhar Singh <singhprakhar359@gmail.com>
2024-02-01 18:35:17 +0530

    created Schema

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+
 const app = express();
 
 app.get('/api/users',(req,res)=>{
@@ -11,5 +12,4 @@ app.get('/',(req,res)=>{
     res.send("hello");
 });
 
- 
 app.listen(5500, ()=> console.log('Server is running on port 5500'));
---
+++ BackEnd/models/Note.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const {Schema} = require('mongoose');
+
+const NoteSchema = new Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    title: {
+       type: String,
+       required : true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    tag: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('note', NotesSchema);
