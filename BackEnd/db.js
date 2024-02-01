bfd71e24b736f2c93bcb6ddd407947b919dc6351
Prakhar Singh <singhprakhar359@gmail.com>
2024-02-01 19:32:57 +0530

    added db.js

---
+++ BackEnd/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+const { MONGOURI } = require('../config/keys');
+const connectToMongo = async () => {
+    try {
+        mongoose.set('strictQuery', false);
+        await mongoose.connect(MONGOURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Connected to MongoDB successfully');
+    } catch (error) {
+        console.log('Error connecting to MongoDB:', error);
+    }
+};
+
+module.exports = connectToMongo;
