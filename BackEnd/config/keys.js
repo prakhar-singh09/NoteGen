0d620994236bf5e22ed0dfe081e259ad65c49910
Prakhar Singh <singhprakhar359@gmail.com>
2024-02-04 13:25:50 +0530

    config update

--- BackEnd/config/keys.js
+++ BackEnd/config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
     JWT_SECRET: process.env.JWT_SECRET,
-    MONGOURI: "mongodb+srv://singhprakhar359:rseymaSAVeJju6I6@cluster0.ychadpx.mongodb.net/?retryWrites=true&w=majority",
+    MONGOURI: process.env.MONGO_URI,
 }
