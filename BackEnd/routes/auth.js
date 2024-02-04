34e2a66e17f1fd144a6f00bb3adeab2180ea0405
Prakhar Singh <singhprakhar359@gmail.com>
2024-02-04 15:59:24 +0530

    model/user

--- BackEnd/routes/auth.js
+++ BackEnd/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
+const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const { body, validationResult } = require('express-validator');
 var jwt = require('jsonwebtoken');
