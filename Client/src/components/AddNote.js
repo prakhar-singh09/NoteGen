031fa177b15b279a81518036d20056935b8b404f
Aman Gupta <90703407+AmanGuptaCoder@users.noreply.github.com>
2024-02-17 01:13:38 +0530

    Update AddNote.js

--- Client/src/components/AddNote.js
+++ Client/src/components/AddNote.js
@@ -91,7 +91,7 @@ const AddNote = () => {
                                 </label>
                             </div>
 
-                            <button disabled={note.title.length < 3 || note.description.length < 5} type="submit" className="btn btn-lg btn-outline-primary mt-2 mb-3 me-2" onClick={handleClick}>
+                            <button  type="submit" className="btn btn-lg btn-outline-primary mt-2 mb-3 me-2" onClick={handleClick}>
                                 Add Note
                             </button>
                             <button  type="submit" className="btn btn-lg btn-outline-primary mt-2 mb-3 me-2" onClick={generateDescription}>
